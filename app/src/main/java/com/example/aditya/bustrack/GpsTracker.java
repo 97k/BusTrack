1585be4854a7dfbe803339926e85a899be7a3f85
Aditya Kaushik <adityakaushik471@gmail.com>
2017-12-27 17:23:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aditya/bustrack/GpsTracker.java
@@ -0,0 +1,41 @@
+package com.example.aditya.bustrack;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by aditya on 12/27/17.
+ */
+
+public class GpsTracker extends Service implements LocationListener {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
