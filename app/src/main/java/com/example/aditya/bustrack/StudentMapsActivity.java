c41ba296d6d48ab4bd49d7bb3c836e703f3df09b
Aditya Kaushik <adityakaushik471@gmail.com>
2018-01-11 02:46:04 +0530

    accepting request_wait

--- app/src/main/java/com/example/aditya/bustrack/StudentMapsActivity.java
+++ app/src/main/java/com/example/aditya/bustrack/StudentMapsActivity.java
@@ -12,6 +12,7 @@ import android.location.Location;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
@@ -34,6 +35,7 @@ import com.firebase.geofire.GeoQuery;
 import com.firebase.geofire.GeoQueryEventListener;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -42,6 +44,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -52,6 +55,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -65,6 +69,8 @@ public class StudentMapsActivity extends AppCompatActivity implements OnMapReady
     DrawerLayout mDrawerLayout;
     @BindView(R.id.nav_view_student)
     NavigationView mNavigationView;
+    @BindView(R.id.locate_bus_fab)
+    FloatingActionButton locateBus;
 
     public static final String LOG_TAG = StudentMapsActivity.class.getSimpleName();
     private static final int RC_PER = 2;
@@ -83,6 +89,7 @@ public class StudentMapsActivity extends AppCompatActivity implements OnMapReady
     private int bus_num;
     private boolean driverFound = false;
     private SharedPreferences prefs;
+    private Marker mBusMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -145,8 +152,8 @@ public class StudentMapsActivity extends AppCompatActivity implements OnMapReady
                             }
 
                             String uId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                            DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Buses").child(String.valueOf(bus_num)).child("studentId");
-                            ref.setValue(uId);
+                            DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Buses").child(String.valueOf(bus_num)).child(uId);
+                            ref.setValue("student");
                             SharedPreferences.Editor editor = prefs.edit();
                             editor.putBoolean(getString(R.string.student_maps_first_time_launch), false);
                             editor.putInt(getString(R.string.bus_no), bus_num);
@@ -170,6 +177,45 @@ public class StudentMapsActivity extends AppCompatActivity implements OnMapReady
 //                getNearestBus();
 //            }
 //        });
+        locateBus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(StudentMapsActivity.this, "Aye aye captain!", Toast.LENGTH_SHORT).show();
+                if (busDriverKey.isEmpty()){
+                    Toast.makeText(StudentMapsActivity.this, "Sorry, Your driver is not online!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                DatabaseReference busLocation = FirebaseDatabase.getInstance().getReference().child("driver_available").child(busDriverKey).child("l");
+                busLocation.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()){
+                            List<Object> map = (List<Object>)dataSnapshot.getValue();
+                            double lat = 0;
+                            double lon = 0;
+                            if (map.get(0) != null){
+                                lat = Double.parseDouble(map.get(0).toString());
+                            }
+
+                            if (map.get(1) != null){
+                                lon = Double.parseDouble(map.get(1).toString());
+                            }
+
+                            LatLng busLocation = new LatLng(lat, lon);
+                            if (mBusMarker != null) mBusMarker.remove();
+                            mBusMarker = mMap.addMarker(new MarkerOptions().position(busLocation).title("Your bus here"));
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
     }
 
     private void setupDrawerContent(NavigationView navigationView) {
@@ -186,6 +232,10 @@ public class StudentMapsActivity extends AppCompatActivity implements OnMapReady
 
                         break;
                     case R.id.request_wait:
+                        if (prefs.getInt(getString(R.string.bus_no), 0)==0){
+                            Toast.makeText(StudentMapsActivity.this, "Please link your bus first!", Toast.LENGTH_LONG).show();
+                            break;
+                        }
                         String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
                         DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("request_wait");
                         GeoFire geofire = new GeoFire(ref);
@@ -213,6 +263,17 @@ public class StudentMapsActivity extends AppCompatActivity implements OnMapReady
                                     map.put("busDriverID", busDriverKey);
                                     ref.child(userId).updateChildren(map);
                                     Log.e(LOG_TAG, "keyis : " + busDriverKey);
+
+                                    String studentId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                                    DatabaseReference driverRef = FirebaseDatabase.getInstance().getReference().child("Users")
+                                            .child("Driver")
+                                            .child(busDriverKey)
+                                            .child("studentRequest");
+                                    driverRef.setValue(studentId);
+
+                                    DatabaseReference locationAdd = FirebaseDatabase.getInstance().getReference().child("Users").child("Students").child(studentId);
+                                    GeoFire geofire = new GeoFire(locationAdd);
+                                    geofire.setLocation(studentId, new GeoLocation(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()));
                                     return;
                                 }
                             }
@@ -265,8 +326,8 @@ public class StudentMapsActivity extends AppCompatActivity implements OnMapReady
                                         }
 
                                         String uId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Buses").child(String.valueOf(bus_num)).child("studentId");
-                                        ref.setValue(uId);
+                                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Buses").child(String.valueOf(bus_num)).child(uId);
+                                        ref.setValue("student");
                                         SharedPreferences.Editor editor = prefs.edit();
                                         editor.putInt(getString(R.string.bus_no), bus_num);
                                         editor.commit();
